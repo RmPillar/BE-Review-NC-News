537e0d85e325c63d837603ea8ae9b7537756ecf2
Robin Pillar <rpillar72@gmail.com>
2019-11-25 15:07:43 +0000

    created util function to convert unix timestamp to date format

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,11 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  formattedDates = [];
+  list.map((item, i) => {
+    formattedDates.push(Object.assign({}, item));
+    formattedDates[i].created_at = new Date(item.created_at);
+  });
+  return formattedDates;
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -2,10 +2,95 @@ const { expect } = require('chai');
 const {
   formatDates,
   makeRefObj,
-  formatComments,
+  formatComments
 } = require('../db/utils/utils');
 
-describe('formatDates', () => {});
+describe.only('formatDates', () => {
+  it('Takes a single input timestamp number and converts it to the date format', () => {
+    const list = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const formattedData = formatDates(list);
+    expect(formattedData[0].created_at).to.be.an.instanceof(Date);
+  });
+  it('Takes an array of multiple items and returns a new array with all create_at timestamps converted to date format', () => {
+    const list = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      },
+      {
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body:
+          'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+        created_at: 1416140514171
+      },
+      {
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
+        created_at: 1289996514171
+      }
+    ];
+    const formattedData = formatDates(list);
+    expect(formattedData[0].created_at).to.be.an.instanceof(Date);
+    expect(formattedData[1].created_at).to.be.an.instanceof(Date);
+    expect(formattedData[2].created_at).to.be.an.instanceof(Date);
+  });
+  it('returns new array', () => {
+    const list = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const formattedData = formatDates(list);
+    expect(formattedData).to.not.equal(list);
+  });
+  it('original array is not mutated', () => {
+    const list = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const expected = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+
+    formatDates(list);
+    expect(list).to.deep.equal(expected);
+  });
+});
 
 describe('makeRefObj', () => {});
 
