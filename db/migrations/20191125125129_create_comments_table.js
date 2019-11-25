d3f22617ad2d225208337d9b7c3760a26e736610
Robin Pillar <rpillar72@gmail.com>
2019-11-25 16:14:44 +0000

    modified article and comment body lengths

--- db/migrations/20191125125129_create_comments_table.js
+++ db/migrations/20191125125129_create_comments_table.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
     commentsTable.integer('article_id').notNullable();
     commentsTable.integer('votes').defaultsTo(0);
     commentsTable.timestamp('created_at').defaultsTo(knex.fn.now());
-    commentsTable.string('body').notNullable();
+    commentsTable.string('body', 1000).notNullable();
     commentsTable
       .foreign('author')
       .references('username')
