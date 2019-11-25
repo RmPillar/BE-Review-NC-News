741a7c1899ce07bf6496fc8c17ecf569bcd0abc5
Robin Pillar <rpillar72@gmail.com>
2019-11-25 12:21:57 +0000

    created topics table up and down migration

--- db/migrations/20191125121505_create_topics_table.js
+++ db/migrations/20191125121505_create_topics_table.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+  console.log('Creating Topics Table');
+  return knex.schema.createTable('topics', topicsTable => {
+    topicsTable
+      .string('slug')
+      .unique()
+      .primary();
+    topicsTable.string('description').notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  console.log('Dropping Topics Table');
+  return knex.schema.dropTable('topics');
 };
