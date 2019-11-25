67e7c5abea79bb33eac583d8f696e8ba1cbb7f18
Robin Pillar <rpillar72@gmail.com>
2019-11-25 12:51:02 +0000

    completed up and down migrations for articles table

--- db/migrations/20191125122237_create_article_table.js
+++ db/migrations/20191125122237_create_article_table.js
@@ -1,8 +1,25 @@
-
 exports.up = function(knex) {
-  
+  console.log('Creating Article Table');
+  return knex.schema.createTable('articles', articleTable => {
+    articleTable.increments('article_id');
+    articleTable.string('title').notNullable();
+    articleTable.string('body').notNullable();
+    articleTable.integer('votes').defaultTo(0);
+    articleTable.string('topic').notNullable();
+    articleTable.string('author').notNullable();
+    articleTable
+      .foreign('topic')
+      .references('slug')
+      .inTable('topics');
+    articleTable
+      .foreign('author')
+      .references('username')
+      .inTable('users');
+    articleTable.timestamp('created_at').defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex) {
-  
+  console.log('Dropping Article Table');
+  return knex.schema.dropTable('articles');
 };
