ae009caedf885f94d76ebad126437448945bd0e6
Robin Pillar <rpillar72@gmail.com>
2019-11-25 14:02:01 +0000

    completed up and down migrations for comments table

--- db/migrations/20191125125129_create_comments_table.js
+++ db/migrations/20191125125129_create_comments_table.js
@@ -1,8 +1,23 @@
-
 exports.up = function(knex) {
-  
+  console.log('Creating Comments Table');
+  return knex.schema.createTable('comments', commentsTable => {
+    commentsTable.increments('comment_id').primary();
+    commentsTable.string('author').notNullable();
+    commentsTable.integer('article_id').notNullable();
+    commentsTable.integer('votes').defaultsTo(0);
+    commentsTable.timestamp('created_at').defaultsTo(knex.fn.now());
+    commentsTable.string('body').notNullable();
+    commentsTable
+      .foreign('author')
+      .references('username')
+      .inTable('users');
+    commentsTable
+      .foreign('article_id')
+      .references('article_id')
+      .inTable('articles');
+  });
 };
 
 exports.down = function(knex) {
-  
+  console.log('Dropping Comments Table');
 };
