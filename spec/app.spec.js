7c320fa4d2e1e329bb94950048a49558609854fd
Robin Pillar <rpillar72@gmail.com>
2019-11-25 16:37:09 +0000

    created app.spec file and added intial dependencies

---
+++ spec/app.spec.js
@@ -0,0 +1,9 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const { expect } = chai;
+const chaiSorted = require('chai-sorted');
+chai.use(chaiSorted);
+const request = require('supertest');
+const app = require('../app');
+const client = require('../db');
