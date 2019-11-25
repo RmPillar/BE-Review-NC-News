fe9ba7214923e1533e180af4f79d5433a4f3e95d
Robin Pillar <rpillar72@gmail.com>
2019-11-25 12:11:52 +0000

    added create user table migration

--- db/migrations/20191125115741_create_user_table.js
+++ db/migrations/20191125115741_create_user_table.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+  console.log('Creating User Table');
+  return knex.schema.createTable('users', usersTable => {
+    usersTable
+      .string('username')
+      .unique()
+      .primary();
+    usersTable.string('avatar_url').notNullable();
+    usersTable.string('name').notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  console.log('Dropping User Table');
 };
