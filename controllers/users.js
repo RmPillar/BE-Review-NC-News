ae68d6addaa73652c3697eba9ba5b1013718103b
Robin Pillar <rpillar72@gmail.com>
2019-11-26 16:30:34 +0000

    modified GEt usersById function to destructure output array

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ exports.getUserById = (req, res, next) => {
   const { username } = req.params;
   fetchUserById(username)
     .then(user => {
-      res.status(200).send({ user });
+      res.status(200).send({ user: user[0] });
     })
     .catch(next);
 };
