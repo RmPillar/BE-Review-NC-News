fea03ecb10eb0b3b4a2e5ff9fecfb58dbdb9c03d
Robin Pillar <rpillar72@gmail.com>
2019-11-26 12:06:41 +0000

    added 500 error handler

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api');
-const { handle404s } = require('./errors');
+const { handle404s, handle500s } = require('./errors');
 
 app.use(express.json());
 app.use('/api', apiRouter);
 
 app.all('/*', handle404s);
+app.use(handle500s);
 
 module.exports = app;
