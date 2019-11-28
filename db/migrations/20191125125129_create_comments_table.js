db3f6e98a32e768788c7e130690a210f4e8bc933
Robin Pillar <rpillar72@gmail.com>
2019-11-28 15:43:03 +0000

    changed references to inline

--- db/migrations/20191125125129_create_comments_table.js
+++ db/migrations/20191125125129_create_comments_table.js
@@ -1,19 +1,17 @@
 exports.up = function(knex) {
   return knex.schema.createTable('comments', commentsTable => {
     commentsTable.increments('comment_id').primary();
-    commentsTable.string('author').notNullable();
-    commentsTable.integer('article_id').notNullable();
+    commentsTable
+      .string('author')
+      .references('.users.username')
+      .notNullable();
+    commentsTable
+      .integer('article_id')
+      .references('articles.article_id')
+      .notNullable();
     commentsTable.integer('votes').defaultsTo(0);
     commentsTable.timestamp('created_at').defaultsTo(knex.fn.now());
     commentsTable.string('body', 1000).notNullable();
-    commentsTable
-      .foreign('author')
-      .references('username')
-      .inTable('users');
-    commentsTable
-      .foreign('article_id')
-      .references('article_id')
-      .inTable('articles');
   });
 };
 
